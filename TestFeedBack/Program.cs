79a6f9489bd3ab503ee854e4b3b26853faf95387
Opilane TTHK <opilane@tthk.ee>
2020-09-15 14:06:10 +0300

    TestFeedBack

---
+++ TestFeedBack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TestFeedBack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what was your test result?");
+            string grade = Console.ReadLine().ToLower();
+
+            switch (grade)
+            { case "a":
+                Console.WriteLine("Excellent!");
+             break;
+             case "b":
+             Console.WriteLine("Very Good");
+                break;
+                case "c":
+                Console.WriteLine("Passed");
+                break;
+                case "d":
+                Console.WriteLine("Failed");
+                break;
+                default:
+                Console.WriteLine("Wasted");
+                break;
+
+        }    }
+    }
+}
